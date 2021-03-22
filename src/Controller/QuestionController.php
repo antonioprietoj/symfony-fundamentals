c19fb22963633ad0d6726150a4e0609e06de270d
antonioprietoj <antonioprietoj98@gmail.com>
2021-03-22 09:47:00 +0100

    final commit

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Service\MarkdownHelper;
 
 use Psr\Log\LoggerInterface;
+use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -41,14 +42,15 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper)
+    public function show($slug, MarkdownHelper $markdownHelper, HubInterface $sentryHub)
     {
+        dump($sentryHub);
         if($this->isDebug){
             $this->logger->info('We are in debug mode!');
         }
-
+        #throw new \Exception('bad stuff happened!');
         $answers = [
-            'Make sure your cat is sitting `purrrfectly still 🤣',
+            'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
--- src/Twig/MarkdownExtension.php
+++ src/Twig/MarkdownExtension.php
@@ -2,31 +2,31 @@
 
 namespace App\Twig;
 
+use App\Service\MarkdownHelper;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class MarkdownExtension extends AbstractExtension
 {
+    private $markdownHelper;
+
+    public function __construct(MarkdownHelper $markdownHelper){
+
+        $this->markdownHelper = $markdownHelper;
+    }
     public function getFilters(): array
     {
         return [
             // If your filter generates SAFE HTML, you should add a third
             // parameter: ['is_safe' => ['html']]
             // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
-            new TwigFilter('filter_name', [$this, 'doSomething']),
+            new TwigFilter('parse_markdown', [$this, 'parseMarkdown'], ['is_safe' => ['html']]),
         ];
     }
 
-    public function getFunctions(): array
+    public function parseMarkdown($value)
     {
-        return [
-            new TwigFunction('function_name', [$this, 'doSomething']),
-        ];
-    }
-
-    public function doSomething($value)
-    {
-        // ...
+        return $this->markdownHelper->parse($value);
     }
 }
